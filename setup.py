7ed98b99c4e5cd5fdc28102c3dccdba7f6375d52
Mohsen DINI <mohsendini1991@gmail.com>
2020-09-22 14:36:22 +0200

    correction d'adress mail

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     description='Reinforcement Learning methods for energy management of microgrids',
     author='Mohsen DINI',
-    author_email='mohsen.dini@gees.centralesupelec.fr',
+    author_email='mohsen.dini@geeps.centralesupelec.fr',
     license='BSD',
     packages=['microgrid_ai'],
     classifiers=["Programming Language :: Python :: 3.6", "Programming Language :: Python :: 3.7"])
